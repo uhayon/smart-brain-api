49081d7f59a7446cfcc3edd460b9b0b8a3b43d76
Uriel Hayon <uri.a77aque@gmail.com>
2019-02-18 01:23:32 -0300

    Change seed hash

--- postgres/seeds/seeds.sql
+++ postgres/seeds/seeds.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
 
 INSERT INTO users (fullname, username, joined) VALUES ('Juan Perez', 'jperez', '2019-02-15');
-INSERT INTO login (hash, username) VALUES ('$2a$10$Wsxdi/QHKHhN.btUVwtPgOsIIKpaalqs3wA/pkpCbrT/wRjPIki5.', 'jperez');
+INSERT INTO login (hash, username) VALUES ('$2a$10$HLLXBhYkxZfbJpdTE3t46e1lXMroiDx4/xrAEampqiMSq7eT7UlNW', 'jperez');
 
 COMMIT;
